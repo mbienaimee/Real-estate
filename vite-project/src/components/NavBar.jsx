8afca9625bc71a8e0bf8517dea9be7542921af32
BIENAIMEE Marie Reine <m.bienaimee@alustudent.com>
2025-02-17 21:24:12 +0200

    project

--- vite-project/src/components/NavBar.jsx
+++ vite-project/src/components/NavBar.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/assets";
 
 function NavBar() {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
   return (
     <div className="absolute top-0 left-0 w-full z-10">
       <div className="container mx-auto flex items-center justify-between py-4 px-6 md:px-20;lg:px-32 bg-transparent">
@@ -23,6 +24,41 @@ function NavBar() {
         <button className="hidden md:block bg-white px-8 py-2 rounded-full">
           Sign up
         </button>
+        <img
+          onClick={() => setShowMobileMenu(true)}
+          src={assets.menu_icon}
+          className="md:hidden w-7 cursor-pointer"
+          alt=""
+        />
+      </div>
+      {/* mobile menu */}
+      <div
+        className={`md:hidden ${
+          showMobileMenu ? "fixed w-full" : "h-0 w-0"
+        }  right-0 top-0 bottom-0 overflow-hidden bg-white transition-all`}
+      >
+        <ul className=" flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium ">
+          <div className="flex justify-end p-6 cursor-pointer">
+            <img
+              onClick={() => setShowMobileMenu(false)}
+              src={assets.cross_icon}
+              className="w-6"
+              alt=""
+            />
+          </div>
+          <a href="" className="px-4 py-2 rounded-full inline-block">
+            Home
+          </a>
+          <a href="" className="px-4 py-2 rounded-full inline-block">
+            About
+          </a>
+          <a href="" className="px-4 py-2 rounded-full inline-block">
+            Projects
+          </a>
+          <a href="" className="px-4 py-2 rounded-full inline-block">
+            Testimonials
+          </a>
+        </ul>
       </div>
     </div>
   );
